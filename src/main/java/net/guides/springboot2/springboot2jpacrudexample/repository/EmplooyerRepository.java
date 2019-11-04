0c23c77aaa7f9992770a781498bc8611a4817353
HerculesGit <hercules.silva@dce.ufpb.br>
2019-11-03 23:29:24 -0300

    Implemented EmployeeRespository

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/repository/EmplooyerRepository.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/repository/EmplooyerRepository.java
@@ -1,5 +1,10 @@
 package net.guides.springboot2.springboot2jpacrudexample.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class EmplooyerRepository {
+import net.guides.springboot2.springboot2jpacrudexample.models.Employee;
+
+@Repository
+public interface EmplooyerRepository extends JpaRepository<Employee, Long>{
 
 }
